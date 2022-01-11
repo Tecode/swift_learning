ac5cdeeee13595a8b0eeae768df1a4a1796d3cd2
haoxuan <283731869@qq.com>
2022-01-11 22:24:46 +0800

    属性、汇编分析inout本质

--- swift_study/swift_study/属性、汇编分析inout本质.swift
+++ swift_study/swift_study/属性、汇编分析inout本质.swift
@@ -39,3 +39,28 @@ enum Person:Int {
 
 print(Person.elong.rawValue)
 // 100
+
+// 取地址赋值，inout本质就是引用传递
+// inout总结
+// 如果实参有物理地址，且没有设置属性观察器
+// 直接将实参的内存地址传入函数（实参进行引用传递）
+
+// 如果实参是计算属性或者设置了属性观察器
+// 采用Copy In Copy Out的做法
+// ✅调用该函数，先复制实参的值，产生副本「get」
+// ✅将副本的内存地址传入函数（副本进行引用传递），在函数内部可以修改副本的值
+// ✅函数返回后，再将副本的值覆盖实参的值「set」
+
+struct Shape {
+    var width:Int
+}
+// 取地址赋值
+func run (_ a: inout Int) {
+    a = 5
+}
+
+var s = Shape(width: 3)
+run(&s.width)
+
+//s.width
+//5
