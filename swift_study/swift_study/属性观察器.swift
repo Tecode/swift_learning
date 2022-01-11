1e761e1b32ba1818b6278311b98385c78b0ddffb
haoxuan <283731869@qq.com>
2022-01-11 21:47:16 +0800

    属性观察器

---
+++ swift_study/swift_study/属性观察器.swift
@@ -0,0 +1,30 @@
+//
+//  属性观察器.swift
+//  swift_study
+//
+//  Created by 昊轩 on 2022/1/11.
+//
+
+import Foundation
+
+struct DriverCar {
+    init () {
+        print("初始化...")
+        self.speed = 10
+    }
+    var speed: Int {
+        willSet {
+            print("设置的新值", newValue)
+        }
+        didSet {
+            print("之前的旧值", oldValue)
+        }
+    }
+}
+
+var d = DriverCar()
+d.speed = 30
+
+//初始化...
+//设置的新值 30
+//之前的旧值 10
