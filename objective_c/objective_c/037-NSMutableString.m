1ae821914f9fd11efa6b62e96d7f7d9c999c6515
haoxuan <283731869@qq.com>
2023-01-03 21:17:49 +0800

    NSMutableString

--- objective_c/objective_c/037-NSMutableString.m
+++ objective_c/objective_c/037-NSMutableString.m
@@ -7,7 +7,21 @@
 
 #import <Foundation/Foundation.h>
 
+// NSMutableString可变字符串
+
 int main() {
-    NSLog(@"OK");
+    NSMutableString *name = [NSMutableString stringWithString: @"haoxuan"];
+//    追加内容
+    [name appendString: @"work in city"];
+//    删除固定位置的字符串
+//    [name deleteCharactersInRange: NSMakeRange(0, 1)];
+//    查找字符串range
+    NSRange range = [name rangeOfString: @"xuan"];
+    [name deleteCharactersInRange: range];
+    
+    NSString *name2 = @"haoxuan";
+    NSString *val = [name2 stringByAppendingString: @"work in home"];
+    NSLog(@"String1: %@", name);
+    NSLog(@"String2: %@", val);
     return 0;
 }
