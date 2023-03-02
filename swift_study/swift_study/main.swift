437681dca813f5d1147ca85851988a8e34908193
haoxuan <283731869@qq.com>
2023-03-02 19:58:31 +0800

    swift字符串

--- swift_study/swift_study/main.swift
+++ swift_study/swift_study/main.swift
@@ -5,5 +5,16 @@
 //  Created by 昊轩 on 2021/12/16.
 //
 
+var name = "haoxuan"
+var list: [Int] = [0, 67]
+name.insert("_", at: name.startIndex)
 
+name.insert(contentsOf: "hh", at: name.endIndex)
+
+name.insert("-", at: name.firstIndex(of: "o")!)
+
+name.append(contentsOf: "IOE")
+print(name)
+// _ha-oxuanhhIOE
+print(list)
 print("OK")
---
+++ swift_study/swift_study/string.swift
@@ -0,0 +1,12 @@
+//
+//  string.swift
+//  swift_study
+//
+//  Created by 昊轩 on 2023/3/2.
+//
+
+import Foundation
+
+var name = "haoxuan"
+
+print(name.count, name.startIndex)
